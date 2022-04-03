c021fb36f5c5ea9bd43c17960661cf1e6546065e
kimyoosang <yoo99485@gmail.com>
2022-04-03 15:29:53 +0900

    [Update] first button test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,12 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import "@testing-library/jest-dom";
 
-test("renders learn react link", () => {
+test("botton has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //find an element with a rile of button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  //expect the background color to be red
+  expect(colorButton).toHaveStyle({ background: "red" });
 });
+test("button turn blue when clicked", () => {});
